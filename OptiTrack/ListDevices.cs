1e366b13d79f6dcf3d6fdda357e96000bc7f0feb
Elliot Woods <elliot@kimchiandchips.com>
2012-09-17 17:02:50 +0100

    using own OptiTrackNET, switching to community version

---
+++ OptiTrack/ListDevices.cs
@@ -0,0 +1,75 @@
+﻿#region usings
+using System;
+using System.ComponentModel.Composition;
+using System.Drawing;
+using System.Threading;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VMath;
+using VVVV.Core.Logging;
+
+using ThreadState = System.Threading.ThreadState;
+using System.Collections.Generic;
+
+using OptiTrackNET;
+
+using VVVV.Nodes.OpenCV;
+
+#endregion usings
+
+namespace VVVV.Nodes.OptiTrack
+{
+	#region PluginInfo
+	[PluginInfo(Name = "ListDevices", Category = "OptiTrack", Help = "List OptiTrack camera devices", Tags = "")]
+	#endregion PluginInfo
+	public class CameraListNode : IPluginEvaluate, IDisposable
+	{
+		#region fields & pins
+		[Input("Refresh", IsBang = true, IsSingle = true)]
+		ISpread<bool> FPinInRefresh;
+
+		[Output("Device")]
+		ISpread<Camera> FPinOutCameras;
+
+		[Import]
+		ILogger FLogger;
+
+		bool FFirstRun = true;
+		OptiTrackNET.Context FContext = new OptiTrackNET.Context();
+
+		#endregion fields & pins
+
+		[ImportingConstructor]
+		public CameraListNode(IPluginHost host)
+		{
+
+		}
+
+		public void Dispose()
+		{
+
+		}
+
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax)
+		{
+			if (FPinInRefresh[0] || FFirstRun)
+			{
+				Refresh();
+				FFirstRun = false;
+			}
+		}
+
+		void Refresh()
+		{
+			var Cameras = Context.Cameras;
+			FPinOutCameras.SliceCount = 0;
+
+			foreach (var Camera in Cameras)
+			{
+				FPinOutCameras.Add(Camera);
+			}
+		}
+	}
+}
---
+++ OptiTrack/VideoIn.cs
@@ -0,0 +1,108 @@
+﻿#region usings
+using System;
+using System.ComponentModel.Composition;
+using System.Drawing;
+using System.Threading;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VMath;
+using VVVV.Core.Logging;
+
+using System.Collections.Generic;
+
+using OptiTrackNET;
+
+using VVVV.Nodes.OpenCV;
+
+#endregion usings
+
+namespace VVVV.Nodes.OptiTrack
+{
+	public class VideoInInstance : IGeneratorInstance
+	{
+		Camera FCamera;
+		public Camera Camera
+		{
+			set
+			{
+				if (value == FCamera)
+					return;
+
+				bool needsRestart = false;
+
+				if (FCamera != null)
+				{
+					FCamera.Stop(true);
+					needsRestart = true;
+				}
+
+				FCamera = value;
+
+				if (needsRestart)
+					this.Start();
+			}
+		}
+
+		protected override bool Open()
+		{
+			try
+			{
+				if (FCamera == null)
+					throw (new Exception("Cannot open camera, no device attached"));
+
+				FCamera.Start();
+				return true;
+			}
+			catch (Exception e)
+			{
+				this.Status = e.Message;
+				return false;
+			}
+		}
+
+		protected override void Close()
+		{
+			FCamera.Stop(true);
+		}
+
+		public override void Allocate()
+		{
+			FOutput.Image.Initialise(FCamera.Size, TColorFormat.L8);
+		}
+
+		protected override void Generate()
+		{
+			return;
+			var frame = FCamera.GetFrame();
+			frame.CopyGrayscaleTo(FOutput.Data);
+		}
+	}
+
+	#region PluginInfo
+	[PluginInfo(Name = "VideoIn", Category = "OptiTrack", Help = "Capture frames from camera device", Tags = "")]
+	#endregion PluginInfo
+	public class VideoInNode : IGeneratorNode<VideoInInstance>
+	{
+		#region fields & pins
+
+		[Input("Device")]
+		IDiffSpread<Camera> FInCamera;
+
+		#endregion
+
+		[ImportingConstructor]
+		public VideoInNode()
+		{
+
+		}
+
+		//called when data for any output pin is requested
+		protected override void Update(int InstanceCount, bool changed)
+		{
+			if (FInCamera.IsChanged)
+				for (int i = 0; i < InstanceCount; i++)
+					FProcessor[i].Camera = FInCamera[i];
+		}
+	}
+}
